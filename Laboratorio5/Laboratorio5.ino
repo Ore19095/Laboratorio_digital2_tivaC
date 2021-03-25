371de748ece678444082553ba47d1d4c1cc94e56
Ore19095 <68124151+Ore19095@users.noreply.github.com>
2021-03-25 14:05:16 -0600

    lee las carpetas

--- Laboratorio5/Laboratorio5.ino
+++ Laboratorio5/Laboratorio5.ino
@@ -1,9 +1,49 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SD.h>
+#include <SPI.h>
 
+
+File directorio;
+String archivos[10];
+
+uint8_t contador = 0; //guarda el numero de archivos que se encuentran 
+
+void verArchivos(File);
+
+
+void setup() {
+  Serial.begin(115200);//iniciar la comunicacion serial 
+  SPI.setModule(0); //uso del modulo spi 
+  Serial.print("Inicializando tarjeta SD...");
+  
+  if (!SD.begin(PA_3)) {
+    Serial.println("Fallo al inicializar");
+    return;
+  }
+  Serial.println("initialization done.");
+
+  directorio = SD.open("/");
+  verArchivos(directorio);
+
+  
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly: 
+
   
+}
+
+
+
+void verArchivos(File directorio){
+  while(true) { //bucle sigue hasta que se de la condicion de salida 
+     File entrada =  directorio.openNextFile();
+     if (!entrada || contador>=10) break; // si ya no hay nada mas o se lleno el buffer de archivos
+
+     if (!entrada.isDirectory()  && contador<10){
+        archivos[contador] = entrada.name();
+        contador++; 
+     }
+     entrada.close();
+   }
 }
