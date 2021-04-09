ca48d9184f67eaad266d0acab660555c4db10574
Ore19095 <68124151+Ore19095@users.noreply.github.com>
2021-04-08 19:08:32 -0600

    commit

--- Laboratorio5/Laboratorio5.ino
+++ Laboratorio5/Laboratorio5.ino
@@ -8,7 +8,7 @@ String archivos[10];
 uint8_t contador = 0; //guarda el numero de archivos que se encuentran 
 
 void verArchivos(File);
-
+void menu();
 
 void setup() {
   Serial.begin(115200);//iniciar la comunicacion serial 
@@ -29,13 +29,54 @@ void setup() {
 }
 
 void loop() {
-
+  menu();
   
 }
 
+void menu(){
+  int opcion;
+  while(true){ //mientras no se seleccione una opcion valida no se sale
+     Serial.println("Seleccione una opcion para visualizar:");
+    for(int i = 0; i<contador; i++){
+      Serial.print(i+1);
+      Serial.print(") ");
+      Serial.println(archivos[i]);
+    }
+    while(! (Serial.available()>0));// esperar a que se envie algo por el serial
+    String entrada = "";
+    while(Serial.available()>0){
+      char caracter = Serial.read();
+      if(caracter !='\n') entrada.concat(caracter);
+      else break; 
+    }
+    opcion = entrada.toInt();
+    if(opcion==0){
+        Serial.println("Opcion no valida");
+        continue;
+      }else{
+        char ruta[50];
+        archivos[opc
+        ion-1].toCharArray(ruta,50);
+        Serial.println(ruta);
+        File abrir = SD.open(ruta);
 
+       if(abrir){
+        while(abrir.available())Serial.write(abrir.read());
+        Serial.println();
+       }
+       else {
+          Serial.println("opcion no valida");
+          continue;
+       }
+        abrir.close();
+      }
 
-void verArchivos(File directorio){
+    
+  }
+ 
+}
+
+void verArchivos(File directorio,){
   while(true) { //bucle sigue hasta que se de la condicion de salida 
      File entrada =  directorio.openNextFile();
      if (!entrada || contador>=10) break; // si ya no hay nada mas o se lleno el buffer de archivos
